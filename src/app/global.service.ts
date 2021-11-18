ada498aa659631bcabb4e4d977c8da95f60272a8
Vladimir Chachi <vchachi@gmail.com>
2021-11-18 11:54:19 -0500

    update new server

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -13,10 +13,10 @@ import {
 // class for some global vars
 export class GlobalService {
     //local for dev
-    public static rootURL: string = "http://localhost:8087"; //server root address Ex. 
+    //public static rootURL: string = "http://localhost:8087"; //server root address Ex. 
 
     //for production
-    //public static rootURL: string = "http://34.136.4.101:8087"; //server root address Ex. 
+    public static rootURL: string = "http://smartchartd.smartsuitetools.com:8087"; 
     //public static rootURL: string = "http://8.209.67.160:8087"; //server root address Ex. 
     //public static rootURL: string = "http://localhost:8087"; //server root address Ex. 
     
